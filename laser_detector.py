dc0dddee000e8835ce2a46a70fe3feae64802630
brandonaccessmemory <116753343+brandonaccessmemory@users.noreply.github.com>
2024-03-31 19:03:11 +0100

    Update laser_detector.py

--- laser_detector.py
+++ laser_detector.py
@@ -6,14 +6,11 @@ from sensor_msgs.msg import LaserScan
 class Laser(Node):
     def __init__(self, node_name: str = 'laser'):
         super().__init__(node_name=node_name)
-        # self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        # self.timer_ = self.create_timer(1.0, self.timer_callback)
-        self.i = 0
         self.scan_sub = self.create_subscription(LaserScan, "scan", self.laser_callback, 10)
         self.get_logger().info("Laser has been initialized.")
         self.scan_sub # Prevent unsued variable warning 
 
-    def laser_callback(self):
+    def laser_callback(self, msg):
         # Process laser scan data 
         ranges = msg.ranges 
         print('Front: ', ranges[0])
@@ -21,7 +18,6 @@ class Laser(Node):
         print('Back: ', ranges[179])
         print('Right: ', ranges[269])
     
-
 
 def main(args=None):
     rclpy.init(args=args)
